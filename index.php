7ac0a8b9164e3722063d6476172d80a44b9baf4e
Jon-Garcia-Tamayo <jgarciatam@educacion.navarra.es>
2021-11-15 14:25:28 +0100

    Colaboradora Naiara: Arreglado baile y terminada la ejecucion

--- index.php
+++ index.php
@@ -4,18 +4,20 @@ include "clases/NsDanza.php";
  use NsDanza\Actividad;
  use NsDanza\Baile;
 
+
  //objetos creados
 $actividad1=new Actividad("", "Exposición Zuloaga", "27-11-2021","Sala de Armas");
 $baile1=new Baile("", "Master Class Zumba ", "28-12-2021","Estadio Perico de Beltrán","Juan Medina Valtierra",42);
-$baile2 = new Baile("","Exposición Zuloaga", "Sala de Armas","27-11-2021","","");
-
+$actividad2 = new Actividad("", "Denomicacion de Origen Navarro", "30-12-2021", "Baluarte");
+$baile2 = new Baile("", "Bailes de Salón", "01-01-2022", "Baluarte", "Maria Lekuona", 27);
 
 //Llamada al método mostrar
 $resultado =""; 
 
 $resultado .= $actividad1->mostrar()."<br>";
 $resultado .= $baile1->mostrar()."<br>";
-$resultado .= $baile2->mostrar();
+$resultado .= $actividad2->mostrar() . "<br>";
+$resultado .= $baile2->mostrar() . "<br>";
 
 include "includes/vista_resultado.php";
 ?>
